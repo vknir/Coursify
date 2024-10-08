bf33b5e2a39cb2ec92fc7bcc92f4256eb2b9ef2c
Utkarsh Kumar <utkarsh.kumar0110@gmail.com>
2024-10-08 11:16:38 +0530

    Backend complete

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,43 +1,36 @@
-const jwt= require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const  {JWT_ADMIN_SECRET}= require('../config')
-const {adminModel} = require('../db.js')
-const { default: mongoose } = require('mongoose')
+const { JWT_ADMIN_SECRET } = require("../config");
+const { adminModel } = require("../db.js");
+const { default: mongoose } = require("mongoose");
 
-async function adminAuth(req, res, next){
-    const token=req.Authorization
+async function adminAuth(req, res, next) {
+  const token = req.headers.authorization;
 
-    try{
-       const decoded= jwt.verify(token, JWT_ADMIN_SECRET)
-       
-       const decodedAdminId= new mongoose.Types.ObjectId( decoded._id)
-       const result = await adminModel.findById(decodedAdminId)
+  try {
+    const decoded = jwt.verify(token, JWT_ADMIN_SECRET);
 
-       // mongoose function returns an array 
-       
-       if(result.length > 0){
+    const decodedAdminId = new mongoose.Types.ObjectId(decoded._id);
+    const result = await adminModel.findById(decodedAdminId);
 
-            req.AdminId= decodedAdminId
-            req.AdminUsername= result[0].username
-        
-            next();
-        }
-        else
-            {
-                res.json({
-                    message:"Token invalid"
-                })
-            }
-       
-       
     
-    }catch(e)
-    {
-        console.log(e)
-        res.json({
-            message:"Token Invalid"
-        })
+
+    if (result) {
+      req.adminId = decodedAdminId;
+      req.adminUsername = result.username;
+
+      next();
+    } else {
+      res.json({
+        message: "Token invalid",
+      });
     }
+  } catch (e) {
+    console.log(e);
+    res.json({
+      message: "Token Invalid",
+    });
+  }
 }
 
-module.exports={adminAuth}
+module.exports = { adminAuth };
--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,11 @@ const adminRouter = Router();
 const { z } = require("zod");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 
 const { JWT_ADMIN_SECRET } = require("../config");
-const { adminModel } = require("../db");
+const { adminModel, courseModel } = require("../db");
+const { adminAuth } = require("../middleware/adminAuth");
 
 adminRouter.use(bodyParser.urlencoded({ extended: false }));
 
@@ -70,20 +72,106 @@ adminRouter.post("/login", async (req, res) => {
           message: "Admin login successful",
           token: token,
         });
+      } else {
+        res.json({ message: "Invalid credential fasle result" });
       }
     });
   } catch (e) {
     console.log(e);
-    res.josn({ message: "Invalid credentails error" });
+    res.json({ message: "Invalid credentails error" });
   }
 });
 
 // auth middleware
 
-adminRouter.post("/courses", (req, res) => {});
+adminRouter.post("/courses", adminAuth, async (req, res) => {
+  const { title, description, price, imageURL, published } = req.body;
+  const adminId = new mongoose.Types.ObjectId(req.adminId);
 
-adminRouter.put("/courses/:courseId", (req, res) => {});
+  const newCourse = {
+    title: title,
+    description: description,
+    price: Number(price),
+    imageURL: imageURL,
+    published: published === "true",
+    adminId: adminId,
+  };
+  //craeting course validator
 
-adminRouter.get("/courses", (req, res) => {});
+  const courseValidator = z.object({
+    title: z.string(),
+    description: z.string(),
+    price: z.number(),
+    imageURL: z.string(),
+    published: z.boolean(),
+  });
+
+  try {
+    courseValidator.parse(newCourse);
+
+    const newCourseAdded = await courseModel.create(newCourse);
+    if (newCourseAdded) {
+      res.json({ message: "Successfully added a new course" });
+    } else {
+      res.json({ message: "Error while updaing DB" });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({ message: "Invalid data type error" });
+  }
+});
+
+adminRouter.put("/courses/:courseId", adminAuth, async (req, res) => {
+  const courseId = new mongoose.Types.ObjectId(req.params.courseId);
+  const adminId = new mongoose.Types.ObjectId(req.adminId);
+
+  const { title, description, price, imageURL, published } = req.body;
+
+  const courseValidator = z.object({
+    title: z.string(),
+    description: z.string(),
+    price: z.number(),
+    imageURL: z.string(),
+    published: z.boolean(),
+  });
+
+  const updatedCourse = {
+    title: title,
+    description: description,
+    price: Number(price),
+    imageURL: imageURL,
+    published: published === "true",
+    adminId: adminId,
+  };
+
+  try {
+    courseValidator.parse(updatedCourse);
+
+    const isCourseUpdated = await courseModel.findByIdAndUpdate(
+      courseId,
+      updatedCourse
+    );
+
+    if (isCourseUpdated) {
+      res.json({ message: "Successfully updated course" });
+    } else {
+      res.json({ message: "Failed to update DB else" });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({ message: "Wrong format error" });
+  }
+});
+
+adminRouter.get("/courses", adminAuth, async (req, res) => {
+  const adminId = new mongoose.Types.ObjectId(req.adminId);
+
+  try {
+    const allCourses = await courseModel.find({ adminId: adminId }).exec();
+    res.json({ allCourses: allCourses });
+  } catch (e) {
+    res.json({ message: "Cannot fetch from db" });
+  }
+});
 
 module.exports = { adminRouter };
